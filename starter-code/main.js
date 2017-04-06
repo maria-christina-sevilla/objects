429ab7aff6ffdbbd36b22e212e84fd7140f93bda
maria-christina-sevilla <m.christina.sevilla@gmail.com>
2017-04-06 10:59:28 -0700

    week 2 homework3

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,48 +1,111 @@
 /*
-	Part 1: We're going to be creating animals today.
-	Create 4 animal objects: a dog, a cat, a bird, a mouse.
-	Each of these animal objects should have a name, a noise that they make,
-	and a makeNoise property that prints out their noise.
+Part 1: 
+	-We're going to be creating animals today.
+	-Create 4 animal objects: a dog, a cat, a bird, a mouse.
+	-Each of these animal objects should have: 
+		-name, 
+		-noise that they make,
+	    -makeNoise property that prints out their noise.
 */
+var dog = {
+    name: "dog",
+    noise: "woof",
+    makeNoise: function() {
+        console.log(this.noise);
+    }
+};
+
+var cat = {
+    name: "cat",
+    noise: "meow",
+    makeNoise: function() {
+        console.log(this.noise);
+    }
+
+};
+
+var bird = {
+    name: bird,
+    noise: "chirp",
+    makeNoise: function() {
+        console.log(this.noise);
+    }
+
+};
+
+var mouse = {
+    name: "mouse",
+    noise: "squeak",
+    makeNoise: function() {
+        console.log(this.noise);
+    }
+
+};
+
+mouse.makeNoise();
 
 /*
-	Part 2: Let's use an array to hold all of these animals.
-	Create a new variable, animals, that contains the 4 animals we just created.
+Part 2: 
+	-Let's use an array to hold all of these animals.
+	-Create a new variable, animals, that contains the 4 animals we just created.
 
-	Use a for-loop to iterate through the array of animals and have each animal make a noise.
+	-Use a for-loop to iterate through the array of animals and have each animal make a noise.
 
-	Hint: Use the makeNoise() function for each animal in the array
+************Hint: Use the makeNoise() function for each animal in the array*****
 */
+var animals = [dog, cat, bird, mouse];
+
+for (var i = 0; i < animals.length; i++) {
+    animals[i].makeNoise();
+}
 
 /*
-	Part 3: Let's see how hungry these animals get by adding a "hunger" property to them.
-	Use a for-loop to iterate through the array of animals and add a "hunger" property with
-	a value of 10. This value will represent how hungry the animal is. A value of 10 means
-	they're pretty full, and a value of 0 means they're starving.
+Part 3: 
+	-Let's see how hungry these animals get by adding a "hunger" property to them.
+	-Use a for-loop to iterate through the array of animals and add a "hunger" property with a value of 10. 
+	-This value will represent how hungry the animal is. 
+	-A value of 10 means they're pretty full, and a value of 0 means they're starving.
 
-	Hint: The syntax to add a new property to an object looks like this:
+	******Hint: The syntax to add a new property to an object looks like this:****
 	myObject.newPropertyName = newValue; // or...
 	myObject['newPropertyName'] = newValue;
 */
+for (var i = 0; i < animals.length; i++) {
+    animals[i].hunger = 10;
+}
+console.log(animals);
 
 /*
-	Part 4: These animals should be able to walk. Let's add a new "walk" property to them.
-	For each animal, the "walk" property should be a function that prints out a string that
-	says they are walking. It should also subtract 1 from their "hunger" property.
+//Part 4: 
+	-These animals should be able to walk. Let's add a new "walk" property to them.
+	-For each animal, the "walk" property should be a function that prints out a string that says they are walking. 
+	-It should also subtract 1 from their "hunger" property.
 
-	For example, if I had a duck that looked like this:
+	-For example, if I had a duck that looked like this:
 	var duck = {
 		name: 'duck',
 		hunger: 8
 	};
 	duck.walk();
-	// => "duck took a walk"
+	 => "duck took a walk"
 //
 	console.log(duck.hunger);
 	// => 7
-
-	Hint: You will need the "this" keyword
+**************Hint: You will need the "this" keyword***************************
 */
+var dog = {
+    name: 'dog',
+    hunger: 8,
+    walk: function() {
+        // your code here
+        animals[i].walk
+        console.log(this.walk)
+    }
+};
+
+dog.walk(); // prints "dog took a walk"
+console.log(dog.hunger); // prints 7
+
 
 /*
 	Part 5: Now these animals can walk and talk by themselves, but they're quite lonely.
@@ -65,6 +128,28 @@
 	duck.friend.makeNoise();
 	// => "chirp quack"
 */
+
+var duck = {
+    name: 'duck',
+    friends: []
+};
+var swan = {
+    name: 'swan',
+    noise: 'chirp quack',
+    friends: []
+};
+
+var makeFriend = function(newFriend) {
+    // your code here
+    // hint: use the 'this' keyword
+};
+
+duck.makeFriend = makeFriend;
+swan.makeFriend = makeFriend;
+
+duck.makeFriend(swan);
+console.log(duck.friends); // prints [{ name: 'swan' }]
+console.log(duck.friends[0].name); // prints 'swan'
 
 
 /* BONUS
@@ -101,4 +186,3 @@ Hint #2: You'll need to use the this keyword to access the name of the current a
 Hint #3: Still stuck? The code to update the image source should look like this:
 	document.querySelector('img').setAttribute('src', 'images/' + this.name + '.jpg');
 */
-	
